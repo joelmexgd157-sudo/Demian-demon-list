16368600e6a1fe6f597843e45fdb16faa40c01d1
joelmexgd157-sudo <joelmex.gd157@gmail.com>
2026-07-09 18:09:43 -0600

    Update fetch path for demons data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 async function cargarDemons() {
-    const respuesta = await fetch("data/demons.json");
+    const respuesta = await fetch("js/data/demons.json");
     const demons = await respuesta.json();
 
     console.log(demons);
